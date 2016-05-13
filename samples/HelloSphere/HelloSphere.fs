08f56d02ff93161970e1d165689b5ac8ae8d8d6a
Kasper Isager <kasperisager@gmail.com>
2016-05-13 13:09:59 +0200

    Rename RaytracerSample -> HelloSphere

--- samples/HelloSphere/HelloSphere.fs
+++ samples/HelloSphere/HelloSphere.fs
@@ -1,12 +1,12 @@
-﻿module RaytracerSample
+﻿module HelloSphere
 
 open Raytracer
 
 type public Program() =
     member this.Main() =
-        let light = mkLight (mkPoint 0.0 0.0 4.0) (mkColour 1. 1. 1.) 1.0
+        let light = mkLight (mkPoint 0.0 0.0 2.0) (mkColour 1. 1. 1.) 1.0
         let ambientLight = mkAmbientLight (mkColour 1. 1. 1.) 0.1
-        let camera = mkCamera (mkPoint 0.0 0.0 4.0) (mkPoint 0.0 0.0 0.0) (mkVector 0.0 1.0 0.0) 1.0 2.0 2.0 500 500
+        let camera = mkCamera (mkPoint 0.0 0.0 2.0) (mkPoint 0.0 0.0 0.0) (mkVector 0.0 1.0 0.0) 1.0 2.0 2.0 500 500
         let sphere = mkSphere (mkPoint 0.0 0.0 0.0) 1.0 (mkMatTexture (mkMaterial (mkColour 0. 0. 1.) 0.0))
         let scene = mkScene [sphere] [light] ambientLight camera 0
 
